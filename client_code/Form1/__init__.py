3e372517fb729f9868ee9bf754fc3ffdff23665d
nicolas.schulz@proscientia.co.uk <nicolas.schulz@proscientia.co.uk>
2023-10-25 13:17:46 +0000

    Created HyperGen-Logo-Icon-POS.png; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 class Form1(Form1Template):
     def __init__(self, **properties):
